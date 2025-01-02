b54ba8225e34af4909303003be4d99e0a23a4561
KumarShresth7 <kumarshresth2004@gmail.com>
2025-01-02 13:20:41 +0530

    home page created

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import MobileNav from '@/components/MobileNav';
 
 const Navbar = () => {
   return (
-    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+    <nav className="flex-between fixed z-50 w-full bg-blue-950 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/icons/logo.svg"
@@ -14,7 +14,7 @@ const Navbar = () => {
           alt="yoom logo"
           className="max-sm:size-10"
         />
-        <p className="text-[26px] font-extrabold text-black max-sm:hidden">
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
           SkyMeet
         </p>
       </Link>
